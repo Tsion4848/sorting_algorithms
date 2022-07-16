71a18c65d9dcabdc5db6baf9de1815faa24395a5
Tsion Moges <tsionmoges25@gmail.com>
2022-07-16 20:19:33 +0300

    bubbole sort

---
+++ 0-bubble_sort.c
@@ -0,0 +1,25 @@
+#include "sort.h"
+/**
+ * bubble_sort - Sort an array with buble sort algorithm
+ * @array: Array that going to be sorted
+ * @size: Size of array
+ * Return: Nothing, just sort and print when swap
+ */
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+
+	for (step = 0 ; step < size - 1 ; ++step)
+	{
+		for (i = 0 ; i < size - step - 1; ++i)
+		{
+			if (array[i] > array[i + 1])
+			{
+				int temp = array[i];
+				array[i] = array[i + 1];
+				array[i + 1] = temp;
+				print_array(array, size);
+			}
+		}
+	}
+}
