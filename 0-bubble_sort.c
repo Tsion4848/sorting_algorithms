f0c0558a925d558fdf2d9453b9fa4b25fc1ee02a
Tsion Moges <tsionmoges25@gmail.com>
2022-07-16 20:25:53 +0300

    bubble sort

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,17 +8,18 @@
 void bubble_sort(int *array, size_t size)
 {
 	size_t i, step;
+	int temp;
 
 	if (array == NULL || size == 0)
 		return;
 
 	for (step = 0 ; step < size - 1 ; step++)
 	{
-		for (i = 0 ; i < size - step - 1; i++)
+		for (i = 0 ; i < size - step - 1 ; i++)
 		{
 			if (array[i] > array[i + 1])
 			{
-				int temp = array[i];
+				temp = array[i];
 				array[i] = array[i + 1];
 				array[i + 1] = temp;
 				print_array(array, size);
